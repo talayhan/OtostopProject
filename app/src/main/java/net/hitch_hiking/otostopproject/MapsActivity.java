5629d9dec59a99982f72a16980f8736d4d0c299d
Samet Sait Talayhan <samet.talayhan@gmail.com>
2015-07-06 12:28:27 +0300

    Added test dataset from firebase.

--- app/src/main/java/net/hitch_hiking/otostopproject/MapsActivity.java
+++ app/src/main/java/net/hitch_hiking/otostopproject/MapsActivity.java
@@ -1,20 +1,32 @@
 package net.hitch_hiking.otostopproject;
 
 import android.app.AlertDialog;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
 
+import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
 import com.firebase.geofire.GeoQuery;
 import com.firebase.geofire.GeoQueryEventListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements GeoQueryEventListener, GoogleMap.OnCameraChangeListener {
 
@@ -27,12 +39,34 @@ public class MapsActivity extends FragmentActivity implements GeoQueryEventListe
     private GeoFire geoFire;
     private GeoQuery geoQuery;
 
+    private Map<String,Marker> markers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
+        // setup map and camera position
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        this.mMap = mapFragment.getMap();
+        LatLng latLngCenter = new LatLng(INITIAL_CENTER.latitude, INITIAL_CENTER.longitude);
+        this.searchCircle = this.mMap.addCircle(new CircleOptions().center(latLngCenter).radius(1000));
+        this.searchCircle.setFillColor(Color.argb(66, 255, 0, 255));
+        this.searchCircle.setStrokeColor(Color.argb(66, 0, 0, 0));
+        this.mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngCenter, INITIAL_ZOOM_LEVEL));
+        this.mMap.setOnCameraChangeListener(this);
+        this.mMap.setMyLocationEnabled(true);
+
+        Firebase.setAndroidContext(this);
+
+        // setup GeoFire
+        this.geoFire = new GeoFire(new Firebase(GEO_FIRE_REF));
+        // radius in km
+        this.geoQuery = this.geoFire.queryAtLocation(INITIAL_CENTER, 1);
+
+        // setup markers
+        this.markers = new HashMap<String, Marker>();
     }
 
     @Override
@@ -80,18 +114,47 @@ public class MapsActivity extends FragmentActivity implements GeoQueryEventListe
     }
 
     @Override
-    public void onKeyEntered(String key, GeoLocation location) {
+    protected void onStop() {
+        super.onStop();
+        // remove all event listeners to stop updating in the background
+        this.geoQuery.removeAllListeners();
+        for (Marker marker: this.markers.values()) {
+            marker.remove();
+        }
+        this.markers.clear();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // add an event listener to start updating locations again
+        this.geoQuery.addGeoQueryEventListener(this);
+    }
+
+    @Override
+    public void onKeyEntered(String key, GeoLocation location) {
+        // Add a new marker to the map
+        Marker marker = this.mMap.addMarker(new MarkerOptions().position(new LatLng(location.latitude, location.longitude)));
+        this.markers.put(key, marker);
     }
 
     @Override
     public void onKeyExited(String key) {
-
+        // Remove any old marker
+        Marker marker = this.markers.get(key);
+        if (marker != null) {
+            marker.remove();
+            this.markers.remove(key);
+        }
     }
 
     @Override
     public void onKeyMoved(String key, GeoLocation location) {
-
+        // Move the marker
+        Marker marker = this.markers.get(key);
+        if (marker != null) {
+            this.animateMarkerTo(marker, location.latitude, location.longitude);
+        }
     }
 
     @Override
@@ -111,6 +174,47 @@ public class MapsActivity extends FragmentActivity implements GeoQueryEventListe
 
     @Override
     public void onCameraChange(CameraPosition cameraPosition) {
-
+        // Update the search criteria for this geoQuery and the circle on the map
+        LatLng center = cameraPosition.target;
+        double radius = zoomLevelToRadius(cameraPosition.zoom);
+        this.searchCircle.setCenter(center);
+        this.searchCircle.setRadius(radius);
+        this.geoQuery.setCenter(new GeoLocation(center.latitude, center.longitude));
+        // radius in km
+        this.geoQuery.setRadius(radius / 1000);
     }
+
+    /**
+     * @param zoomLevel control zoomlevel 2^zoomLevel */
+    private double zoomLevelToRadius(double zoomLevel) {
+        // Approximation to fit circle into view
+        return 16384000/Math.pow(2, zoomLevel);
+    }
+
+    // Animation handler for old APIs without animation support
+    private void animateMarkerTo(final Marker marker, final double lat, final double lng) {
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        final long DURATION_MS = 3000;
+        final Interpolator interpolator = new AccelerateDecelerateInterpolator();
+        final LatLng startPosition = marker.getPosition();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                float elapsed = SystemClock.uptimeMillis() - start;
+                float t = elapsed/DURATION_MS;
+                float v = interpolator.getInterpolation(t);
+
+                double currentLat = (lat - startPosition.latitude) * v + startPosition.latitude;
+                double currentLng = (lng - startPosition.longitude) * v + startPosition.longitude;
+                marker.setPosition(new LatLng(currentLat, currentLng));
+
+                // if animation is not finished yet, repeat
+                if (t < 1) {
+                    handler.postDelayed(this, 16);
+                }
+            }
+        });
+    }
+
 }
